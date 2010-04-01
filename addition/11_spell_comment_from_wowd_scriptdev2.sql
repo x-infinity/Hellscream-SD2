0e6b50921c96f8cc4d53ee6083750c1aa4f4190d
Cristy <cojocaru_cristi2000@yahoo.com>
2010-04-01 19:51:51 +0300

    [12] TOC class bugfixes. Patch provided by rsa

--- addition/11_spell_comment_from_wowd_scriptdev2.sql
+++ addition/11_spell_comment_from_wowd_scriptdev2.sql
@@ -1,7 +1,7 @@
--- Insert comments to scriptdev2 boss_spell_table from WOWD database. 
+-- Insert comments to scriptdev2 boss_spell_table from WOWD database.
 -- Change WOWD database name if you use this!
-
-CREATE ALGORITHM = TEMPTABLE VIEW `commentlist` 
+ 
+CREATE ALGORITHM = TEMPTABLE VIEW `commentlist`
 (`entry` ,`spell`, `comment`)
 AS SELECT `scriptdev2`.`boss_spell_table`.`entry`,
 `spellID_N10`,
@@ -9,10 +9,11 @@ CONCAT(`mangos`.`creature_template`.`name`,
 ' : ',
 `mangos`.`wowd_spell`.`SpellName`)
 FROM `scriptdev2`.`boss_spell_table`
-INNER JOIN `mangos`.`creature_template` ON `mangos`.`creature_template`.`entry` = `scriptdev2`.`boss_spell_table`.`entry` 
+INNER JOIN `mangos`.`creature_template` ON `mangos`.`creature_template`.`entry` = `scriptdev2`.`boss_spell_table`.`entry`
 INNER JOIN `mangos`.`wowd_spell` ON `mangos`.`wowd_spell`.`id` = `scriptdev2`.`boss_spell_table`.`spellID_N10`;
-
-UPDATE `scriptdev2`.`boss_spell_table` SET `comment` =  (SELECT DISTINCT `commentlist`.`comment`
-FROM `commentlist` WHERE `scriptdev2`.`boss_spell_table`.`entry` = `commentlist`.`entry` 
-AND `scriptdev2`.`boss_spell_table`.`spellID_N10` = `commentlist`.`spell`);
+ 
+UPDATE `scriptdev2`.`boss_spell_table` SET `comment` = (SELECT DISTINCT `commentlist`.`comment`
+FROM `commentlist` WHERE `scriptdev2`.`boss_spell_table`.`entry` = `commentlist`.`entry`
+AND `scriptdev2`.`boss_spell_table`.`spellID_N10` = `commentlist`.`spell`
+AND `scriptdev2`.`boss_spell_table`.`comment` = NULL);
 DROP VIEW `commentlist`;
