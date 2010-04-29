54afb300155aca7eec2f6318d46471fa09874e12
Dark <dark_rulerz@hotmail.com>
2010-04-29 13:36:54 +0200

    Major SQL rename + replace to fit our core GIT
    Signed-off-by: Dark <dark_rulerz@hotmail.com>

--- sql/Hellscream_Fixes/SD2/boss_spell_table_scriptdev2.sql
+++ sql/Hellscream_Fixes/SD2/boss_spell_table_scriptdev2.sql
@@ -31,3 +31,23 @@ CREATE TABLE IF NOT EXISTS `boss_spell_table` (
   PRIMARY KEY (`entry`,`spellID_N10`,`CastType`),
   INDEX `idx_entry`(`entry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT 'Boss spell table by /dev/rsa';
+
+-- Insert comments to scriptdev2 boss_spell_table from WOWD database. 
+-- Change WOWD database name if you use this!
+
+CREATE ALGORITHM = TEMPTABLE VIEW `commentlist` 
+(`entry` ,`spell`, `comment`)
+AS SELECT `scriptdev2`.`boss_spell_table`.`entry`,
+`spellID_N10`,
+CONCAT(`mangos`.`creature_template`.`name`,
+' : ',
+`mangos`.`wowd_spell`.`SpellName`)
+FROM `scriptdev2`.`boss_spell_table`
+INNER JOIN `mangos`.`creature_template` ON `mangos`.`creature_template`.`entry` = `scriptdev2`.`boss_spell_table`.`entry` 
+INNER JOIN `mangos`.`wowd_spell` ON `mangos`.`wowd_spell`.`id` = `scriptdev2`.`boss_spell_table`.`spellID_N10`;
+
+UPDATE `scriptdev2`.`boss_spell_table` SET `comment` =  (SELECT DISTINCT `commentlist`.`comment`
+FROM `commentlist` WHERE `scriptdev2`.`boss_spell_table`.`entry` = `commentlist`.`entry` 
+AND `scriptdev2`.`boss_spell_table`.`spellID_N10` = `commentlist`.`spell`
+AND `scriptdev2`.`boss_spell_table`.`comment` IS NULL);
+DROP VIEW `commentlist`;
